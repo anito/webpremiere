f548481b84991c3c6d8b8bcbd4d8818a08153c8e
anito <axelnitzschner@mac.com>
2018-10-05 14:05:58 +0200

    impreza child

---
+++ wp-content/themes/Impreza-child/functions.php
@@ -0,0 +1,4 @@
+<?php
+require_once( __DIR__ . '/includes/product_category_handler.php');
+require_once( __DIR__ . '/includes/duplicate_content.php');
+require_once( __DIR__ . '/includes/sender_email.php');
