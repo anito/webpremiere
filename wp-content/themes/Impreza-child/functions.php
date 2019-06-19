df5da16e33d4c18aaedab521cb43e3ae926a0b7e
anito <axelnitzschner@mac.com>
2019-06-19 15:37:39 +0200

    changes

--- wp-content/themes/Impreza-child/functions.php
+++ wp-content/themes/Impreza-child/functions.php
@@ -5,6 +5,15 @@ require_once( __DIR__ . '/includes/sender_email.php');
 
 add_filter( 'woocommerce_allow_marketplace_suggestions', '__return_false' );
 
+/*
+ * Load Translations
+ */
+function child_theme_slug_setup() {
+    
+    load_child_theme_textdomain( 'us', get_stylesheet_directory() . '/common/languages' );
+    
+}
+add_action( 'after_setup_theme', 'child_theme_slug_setup' );
 
 /* ----------------------------------------------------------------------------------- */
 /* Display a Backup Warning
