38cad3c9b07c869319c6508ebaf5ea131a76be92
anito <axelnitzschner@mac.com>
2019-06-19 18:12:40 +0200

    changes

--- wp-content/themes/Impreza-child/wp-maintenance-mode.php
+++ wp-content/themes/Impreza-child/wp-maintenance-mode.php
@@ -45,14 +45,16 @@ $social_prefix = 'social_';
 
 <body class="<?php echo $body_classes ? $body_classes : ''; ?>">
     <div class="container">
+        
+        <div class="container">
 
-        <header class="header">
-            <h1><?php echo $texts['header']; ?></h1>
-            <h2><?php echo $texts['subheader']; ?></h2>
-        </header>
+            <header class="header">
+                <h1><?php echo $texts['header']; ?></h1>
+                <h2><?php echo $texts['subheader']; ?></h2>
+            </header>
 
-        <!--START_TIMER_BLOCK-->
-        <?php if ( $this->plugin_settings['modules']['countdown_status'] ): ?>
+            <!--START_TIMER_BLOCK-->
+            <?php if ( $this->plugin_settings['modules']['countdown_status'] ): ?>
             <section class="timer">
                 <div class="timer__item">
                     <div class="timer__data" id="timerResultDays"></div>
@@ -74,34 +76,78 @@ $social_prefix = 'social_';
             <script type="application/javascript">
                 startTimer(<?php echo $days . ',' . $hours . ',' . $minutes . ',' . strtotime($countdown_start) . ',' . time(); ?> - Math.floor(Date.now() / 1000));
             </script>
-        <?php endif; ?>
-        <!--END_TIMER_BLOCK-->
+            <?php endif; ?>
+            <!--END_TIMER_BLOCK-->
 
-        <!--START_SOCIAL_LINKS_BLOCK-->
-        <section class="social-links">
-            <?php foreach($this->plugin_settings['modules'] as $network => $url): ?>
-                <?php $social = (($test = preg_replace('/'.$social_prefix.'(\w+)/', '$1', $network)) === $network) ?
-                    FALSE : $test ?>
-                <?php if( $social &&  in_array($social, $allowed_socials) && !empty($url) ) { ?>
-                <a class="social-links__link" href="<?php echo $url;?>" target="_blank" title="<?php echo ucfirst($social);?>">
-                    <span class="icon"><img src="<?php echo $assetsUrl; ?>images/<?php echo $social;?>.svg" alt="<?php echo ucfirst($social);?>"></span>
-                </a>
-                <?php  } ?>
-            <?php endforeach; ?>
-        </section>
-        <!--END_SOCIAL_LINKS_BLOCK-->
+            <!--START_SOCIAL_LINKS_BLOCK-->
+            <?php if (!empty($this->plugin_settings['modules']['social_status']) && $this->plugin_settings['modules']['social_status'] == 1) { ?>
+            <section class="social-links">
+                <?php foreach($this->plugin_settings['modules'] as $network => $url): ?>
+                    <?php $social = (($test = preg_replace('/'.$social_prefix.'(\w+)/', '$1', $network)) === $network) ?
+                        FALSE : $test ?>
+                    <?php if( $social &&  in_array($social, $allowed_socials) && !empty($url) ) { ?>
+                    <a class="social-links__link" href="<?php echo $url;?>" target="_blank" title="<?php echo ucfirst($social);?>">
+                        <span class="icon"><img src="<?php echo $assetsUrl; ?>images/<?php echo $social;?>.svg" alt="<?php echo ucfirst($social);?>"></span>
+                    </a>
+                    <?php  } ?>
+                <?php endforeach; ?>
+            </section>
+            <?php } ?>
+            <!--END_SOCIAL_LINKS_BLOCK-->
 
+
+            <?php if (!empty($this->plugin_settings['bot']['status']) && $this->plugin_settings['bot']['status'] === 1) { ?>
+
+            <section class="bot-container">
+                <!-- WP Bot -->
+                <div class="bot-chat-wrapper">
+                    <!-- Chats -->
+                    <div class="chat-container cf"></div>
+                    <!-- User input -->
+                    <div class="input"></div>
+                    <!-- User choices -->
+                    <div class="choices cf"></div>
+                </div>
+                <!-- /WP Bot -->
+            </section>
+            <div class="bot-error"><p></p></div>
+        </div>
     </div>
-
+    
     <footer class="footer">
         <div class="footer__bg_copyright"><?php echo $credits; ?></div>
         <div class="footer__content">
-            <?php printf( __( '%1$s is proudly powered by %2$s', 'us' ), get_bloginfo('name'), '<a href="https://webpremiere.de/" target="_blank"><img class="logo" src="https://webpremiere.de/files/public-docs/webpremiere/webpremiere-logo-5.svg" alt="WebPremiere"></a><sup>&reg;</sup>' ); ?>
+            <?php printf(__('%1$s is proudly powered by %2$s', 'us'), get_bloginfo('name'), '<a href="https://webpremiere.de/" target="_blank"><img class="logo" src="https://webpremiere.de/files/public-docs/webpremiere/webpremiere-logo-5.svg" alt="WebPremiere"></a><sup>&reg;</sup>'); ?>
         </div>
     </footer>
-
+    
+    <?php } ?>
     <script type='text/javascript'>
 			var wpmm_vars = {"ajax_url": "<?php echo admin_url('admin-ajax.php'); ?>"};
     </script>
+    
+    <?php
+		
+    // Hook before scripts, mostly for internationalization
+    do_action('wpmm_before_scripts');
+
+    if (!empty($scripts) && is_array($scripts)) {
+        foreach ($scripts as $src) {
+            ?>
+            <script src="<?php echo $src; ?>"></script>
+            <?php
+        }
+    }
+    // Do some actions
+    do_action('wpmm_footer');
+    ?>
+    
+    <?php if (!empty($this->plugin_settings['bot']['status']) && $this->plugin_settings['bot']['status'] === 1) { ?>
+        <script type='text/javascript'>
+            jQuery(function($) {
+                startConversation('homepage', 1);
+            });
+        </script>
+    <?php } ?>
 </body>
 </html>
