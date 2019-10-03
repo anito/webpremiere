5327566d584d6ef83b9fd24c6f0b9ef457018300
anito <axelnitzschner@mac.com>
2019-10-03 14:24:49 +0200

    cookie policy

--- wp-content/themes/Impreza-child/functions.php
+++ wp-content/themes/Impreza-child/functions.php
@@ -16,14 +16,14 @@ function child_theme_slug_setup() {
 add_action( 'after_setup_theme', 'child_theme_slug_setup' );
 
 // Cookie Policy Settings
-add_action( 'wp_head', 'add_cookie_policy' );
+// add_action( 'wp_head', 'add_cookie_policy' );
 function add_cookie_policy() {
 	?>
 	<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="efe535a2-5b4a-41c9-a3c9-cd065404fbb2" data-blockingmode="auto" type="text/javascript"></script>
 	<?php
 }
 
-add_action( 'wp_footer', 'display_cookie_policy' );
+// add_action( 'wp_footer', 'display_cookie_policy' );
 function display_cookie_policy() {
 	?>
 	<script id="CookieDeclaration" src="https://consent.cookiebot.com/efe535a2-5b4a-41c9-a3c9-cd065404fbb2/cd.js" type="text/javascript" async></script>
